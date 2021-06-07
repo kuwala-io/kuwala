1e6418d71b807e198cb49474b5e868eceb0459bf
ibrahim-bouncoula <373982+bob-bn@users.noreply.github.com>
2021-06-08 00:31:42 +0200

    fix(pythonpath): enriched  sys path for python with more module locations

--- kuwala-pipelines/google-poi/src/app.py
+++ kuwala-pipelines/google-poi/src/app.py
@@ -1,6 +1,10 @@
 import logging
 import os
 import sys
+
+sys.path.insert(0, '../../common/')
+sys.path.insert(0, '../')
+
 from dotenv import load_dotenv
 from quart import Quart
 from routes.search import search
