d3685ca8de65fe6e116d72f011d77e62f670051a
Matti Grotheer <matti@kuwala.io>
2021-06-21 20:17:30 +0200

    Save closed tags of POIs in graph

--- kuwala/core/neo4j/importer/src/PoiGoogleImporter.py
+++ kuwala/core/neo4j/importer/src/PoiGoogleImporter.py
@@ -1,4 +1,5 @@
 import h3
+import moment
 import os
 import Neo4jConnection as Neo4jConnection
 from pyspark.sql import DataFrame, SparkSession
@@ -10,6 +11,9 @@ def add_constraints():
     # TODO: Create alternative constraint for community version (Node key only available in Neo4j Enterprise)
     Neo4jConnection.query_graph('CREATE CONSTRAINT poiOpeningHours IF NOT EXISTS ON (p:PoiOpeningHours) ASSERT '
                                 '(p.openingTime, p.closingTime) IS NODE KEY')
+    # TODO: Create alternative constraint for community version (Node key only available in Neo4j Enterprise)
+    Neo4jConnection.query_graph('CREATE CONSTRAINT poiClosed IF NOT EXISTS ON (p:PoiClosed) ASSERT '
+                                '(p.closed, p.permanently) IS NODE KEY')
 
 
 def add_google_pois(df: DataFrame):
@@ -50,6 +54,24 @@ def add_opening_hours(df: DataFrame):
     df.foreachPartition(lambda p: Neo4jConnection.batch_insert_data(p, query))
 
 
+def add_closed_tags(df: DataFrame):
+    Neo4jConnection.query_graph('''
+        MERGE (:PoiClosed { closed: false, permanently: false })
+        MERGE (:PoiClosed { closed: true, permanently: false })
+        MERGE (:PoiClosed { closed: true, permanently: true })
+    ''')
+
+    query = '''
+        UNWIND $rows AS row
+        MATCH (pg:PoiGoogle { id: row.id })
+        MATCH (pc:PoiClosed)
+        WHERE pc.closed = (row.permanentlyClosed OR row.temporarilyClosed) AND pc.permanently = row.permanentlyClosed
+        MERGE (pg)-[:IS { date: row.date }]->(pc)
+    '''
+
+    df.foreachPartition(lambda p: Neo4jConnection.batch_insert_data(p, query))
+
+
 def import_pois_google(limit=None):
     Neo4jConnection.connect_to_graph(uri="bolt://localhost:7687",
                                      user="neo4j",
@@ -68,16 +90,20 @@ def import_pois_google(limit=None):
 
     # noinspection PyUnresolvedReferences
     resolution = h3.h3_get_resolution(df.first()['h3Index'])
-    google_pois = df \
-        .select('id', 'h3Index', 'name', 'placeID', 'address', 'timezone', 'contact.*') \
+    time_zone = df.first()['timezone']
+    date = moment.utcnow().timezone(time_zone).replace(hours=0, minutes=0, seconds=0)
+
+    google_pois = df.select('id', 'h3Index', 'name', 'placeID', 'address', 'timezone', 'contact.*') \
         .withColumn('resolution', lit(resolution))
     opening_hours = df \
         .select('id', 'openingHours') \
         .withColumn('openingHours', explode('openingHours')) \
         .select('id', 'openingHours.*') \
         .filter(col('closingTime').isNotNull() & col('openingTime').isNotNull())
+    closed_tags = df.select('id', 'permanentlyClosed', 'temporarilyClosed').withColumn('date', lit(str(date)))
 
     add_google_pois(google_pois)
     add_opening_hours(opening_hours)
+    add_closed_tags(closed_tags)
 
     spark.stop()
