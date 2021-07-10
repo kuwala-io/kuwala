f977a75c935629df0fd886877e72782588161b09
Matti Grotheer <matti@kuwala.io>
2021-07-10 17:54:19 +0200

    Optional argument for polygon coordinates to limit region of Google scraper

---
+++ kuwala/pipelines/common/python_utils/src/h3_utils.py
@@ -0,0 +1,10 @@
+import geojson
+import h3
+
+
+def polyfill_polygon(polygon: geojson.Polygon, resolution):
+    h3_indexes = h3.polyfill(dict(type=polygon.type, coordinates=polygon.coordinates),
+                             resolution,
+                             geo_json_conformant=True)
+
+    return h3_indexes
