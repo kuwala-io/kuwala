4871e0551856a9a09ca24220530d0cc45deec6ca
Matti Grotheer <matti@kuwala.io>
2021-07-12 18:25:19 +0200

    Cypher UDF to get hour of timestamp

--- kuwala/core/neo4j/plugins/udfs/src/main/java/io/kuwala/time/Time.java
+++ kuwala/core/neo4j/plugins/udfs/src/main/java/io/kuwala/time/Time.java
@@ -1,5 +1,8 @@
-package io.kuwala.moment;
+package io.kuwala.time;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
@@ -9,9 +12,9 @@ import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 
-public class Moment {
+public class Time {
     @UserFunction
-    @Description("io.kuwala.moment.durationHours(start, end) - Returns the duration of two datetimes in hours.")
+    @Description("io.kuwala.time.durationHours(start, end) - Returns the duration of two datetimes in hours.")
     public double durationHours(
             @Name("start") String start,
             @Name("end") String end
@@ -29,6 +32,15 @@ public class Moment {
         }
 
         return duration;
+    }
+
+    @UserFunction
+    @Description("io.kuwala.time.getHour(datetime) - Returns the hour of a given datetime.")
+    public double getHour(@Name("datetime") String datetime) {
+        DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ssZ").withOffsetParsed();
+        DateTime dt = fmt.parseDateTime(datetime.replace(".", ""));
+
+        return dt.getHourOfDay();
     }
 }
 
