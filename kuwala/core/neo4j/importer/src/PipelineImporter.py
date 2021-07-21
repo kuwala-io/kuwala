9afe3f703f518b1b59bf7fdfd9d27ad5cff2ca2c
Matti Grotheer <matti@kuwala.io>
2021-07-21 18:54:08 +0200

    Import population Parquet into Neo4j

--- kuwala/core/neo4j/importer/src/PipelineImporter.py
+++ kuwala/core/neo4j/importer/src/PipelineImporter.py
@@ -1,4 +1,5 @@
 import os
+import Neo4jConnection as Neo4jConnection
 from PipelineConnector import connect_h3_indexes, connect_pois
 from PoiGoogleImporter import import_pois_google
 from PoiOSMImporter import import_pois_osm
@@ -24,11 +25,16 @@ def connect_to_mongo(database, collection):
 def start():
     SparkSession.builder.config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1')
 
+    # Create general H3 constraint
+    Neo4jConnection.connect_to_graph()
+    Neo4jConnection.query_graph('CREATE CONSTRAINT h3Index IF NOT EXISTS ON (h:H3Index) ASSERT h.h3Index IS UNIQUE')
+    Neo4jConnection.close_connection()
+
     # Import data
+    import_population_density()
+
     df_pois_osm = import_pois_osm()
     df_pois_google = import_pois_google()
-
-    import_population_density()
 
     # Connect data
     connect_pipelines(df_pois_osm, df_pois_google)
