ba7770d4a648484db13749ac8fc7f17397ac0b64
Matti Grotheer <matti@kuwala.io>
2021-07-26 00:06:22 +0200

    UDF to get H3 neighbors within radius

--- kuwala/core/neo4j/plugins/udfs/src/test/java/H3Test.java
+++ kuwala/core/neo4j/plugins/udfs/src/test/java/H3Test.java
@@ -1,9 +1,11 @@
 import io.kuwala.h3.H3;
+import org.apache.commons.lang.StringUtils;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.*;
 import org.neo4j.driver.*;
 import org.neo4j.harness.Neo4j;
 import org.neo4j.harness.Neo4jBuilders;
+import java.util.List;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -38,9 +40,21 @@ public class H3Test {
 
     @Test
     public void shouldReturnParentIndex() {
-            Session session = driver.session();
-            String result = session.run( "RETURN io.kuwala.h3.h3ToParent('8f3f3040004caca', 11) AS result").single().get("result").asString();
+        Session session = driver.session();
+        String result = session.run( "RETURN io.kuwala.h3.h3ToParent('8f3f3040004caca', 11) AS result").single().get("result").asString();
 
-            MatcherAssert.assertThat( result, equalTo("8b3f3040004cfff") );
+        MatcherAssert.assertThat( result, equalTo("8b3f3040004cfff") );
+    }
+
+    @Test
+    public void shouldReturnNeighborsInRadius() {
+        Session session = driver.session();
+        List<String> result = session
+                .run( "RETURN io.kuwala.h3.getNeighborsInRadius('8b3f30453725fff', 11, 100) AS result")
+                .single()
+                .get("result")
+                .asList(Value::asString);
+
+        MatcherAssert.assertThat(StringUtils.join(result, ','), equalTo("8b3f30453725fff,8b3f30453724fff,8b3f30453720fff,8b3f30453721fff,8b3f304530d2fff,8b3f304530d6fff,8b3f3045308bfff,8b3f3045308afff,8b3f30453099fff,8b3f30453726fff,8b3f30453722fff,8b3f30453723fff,8b3f3045372efff,8b3f3045372cfff,8b3f304530d3fff,8b3f304530d0fff,8b3f304530d4fff,8b3f30453089fff,8b3f30453088fff"));
     }
 }
