c8b3edc251fff2c9ec4b98c00b7827f170c702af
Iga Narendra <iga.narendra@gmail.com>
2021-12-21 21:06:09 +0700

    add args in parser

--- kuwala/core/neo4j/importer/src/main.py
+++ kuwala/core/neo4j/importer/src/main.py
@@ -7,6 +7,7 @@ import PipelineImporter as PipelineImporter
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--date',help='Selected update date to process')
     parser.add_argument('--continent', help='Continent of the file')
     parser.add_argument('--country', help='Country of the file')
     parser.add_argument('--country_region', help='Country region of the file')
