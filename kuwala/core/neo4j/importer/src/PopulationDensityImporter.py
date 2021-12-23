6189522890a7afa3e68ec729b82e1b4c204bba6f
IritaSee <iga.narendra@gmail.com>
2021-12-23 13:46:41 +0700

    fix date parsing

--- kuwala/core/neo4j/importer/src/PopulationDensityImporter.py
+++ kuwala/core/neo4j/importer/src/PopulationDensityImporter.py
@@ -41,13 +41,13 @@ def import_population_density(args, limit=None):
     if continent is None or country is None:
         try:
             file_path = select_local_country(file_path)
-            file_path += '/result'+date+'.parquet' #fix this 
+            file_path += '/result'+'_'+date+'.parquet' 
         except FileNotFoundError:
             print('No population data available for import')
 
             return
     else: 
-        file_path += f'{continent}/{country}/result_{date}.parquet' #fix this
+        file_path += f'{continent}/{country}/result_{date}.parquet'
 
     if not os.path.exists(file_path):
         print('No population data available for import')
