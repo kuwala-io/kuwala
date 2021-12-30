effb07b64164e5836f1046d43b5d023582f1605d
Iga Narendra <iga.narendra@gmail.com>
2021-12-30 16:34:07 +0700

    re adjusting database importer

--- kuwala/core/neo4j/importer/src/PopulationDensityImporter.py
+++ kuwala/core/neo4j/importer/src/PopulationDensityImporter.py
@@ -41,13 +41,13 @@ def import_population_density(args, limit=None):
     if continent is None or country is None:
         try:
             file_path = select_local_country(file_path)
-            file_path += '/result'+'_'+date+'.parquet' 
+            file_path += '/'+ date +'_result.parquet' 
         except FileNotFoundError:
             print('No population data available for import')
 
             return
     else: 
-        file_path += f'{continent}/{country}/result_{date}.parquet'
+        file_path += f'{continent}/{country}/{date}_result.parquet'
 
     if not os.path.exists(file_path):
         print('No population data available for import')
