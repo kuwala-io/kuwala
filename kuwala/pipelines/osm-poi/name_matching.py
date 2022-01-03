b74d373897534145cd0eccd39b0658c63f06a04b
IritaSee <iga.narendra@gmail.com>
2022-01-03 17:26:05 +0700

    [undone] interating over json

--- kuwala/pipelines/osm-poi/name_matching.py
+++ kuwala/pipelines/osm-poi/name_matching.py
@@ -1,6 +1,7 @@
 import urllib.request as req
 import zipfile
 import os
+import json
 
 
 # here, instead of cloning the repository that recommended using extra library,
@@ -12,4 +13,22 @@ if not os.path.exists('./tmp/name-suggestion-index-main'):
         zip_ref.extractall('./tmp/')
     os.remove('./tmp/main.zip')
 
+file_paths='./tmp/name-suggestion-index-main/data/brands'
+data = {'id': [], 'display_name': [], 'wiki_data': []}
+for folders in os.listdir(file_paths):
+    #print(folders)
+    if os.path.isdir(os.path.join(file_paths,folders)):
+        for files in os.listdir(os.path.join(file_paths,folders)):
+            #print(files)
+            f=open(os.path.join(file_paths,folders,files))
+            file_content=json.load(f)
+            for a in file_content['items'] : 
+                print(a)
+                #print(a)
+                # if a['id'] == [] : break
+                # data['id'].append(a['id'])
+                # data['display_name'].append(a['displayName'])
 
+
+
+print(data)
