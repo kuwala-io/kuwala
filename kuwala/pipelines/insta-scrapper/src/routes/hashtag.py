5e5c448e43cf121c4959d241a74a20ba0964c291
Arif Luthfi <arifluthfi16@gmail.com>
2022-01-22 22:37:12 +0700

    feat(insta-scrapper): changed hashtag from urls to search queries

--- kuwala/pipelines/insta-scrapper/src/routes/hashtag.py
+++ kuwala/pipelines/insta-scrapper/src/routes/hashtag.py
@@ -6,14 +6,19 @@ hashtag = Blueprint('hashtag', __name__, url_prefix='/hashtag')
 
 @hashtag.route('/', methods=['GET'])
 async def get_hashtag_information():
-    urls = await request.get_json()
+    search_tags = await request.get_json()
     
-    if urls is None:
+    if search_tags is None:
         abort(400, description='Invalid request body, is the request body type a JSON?')
 
-    if len(urls) > 100:
+    if len(search_tags) > 100:
         abort(400, description='You can send at most 100 ids at once.')
+
+    urls = list(map(convert_into_url_list, search_tags))
 
     result = processor.process_by_type(urls, 'hashtag')
 
     return jsonify(result)
+
+def convert_into_url_list(hashtag):
+    return f'https://www.instagram.com/explore/tags/{hashtag.lower()}/'
