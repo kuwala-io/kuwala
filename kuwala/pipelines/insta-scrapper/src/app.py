381beb6ad88ca79184c3976e298be6984856a7cd
Arif Luthfi <arifluthfi16@gmail.com>
2022-01-22 23:51:33 +0700

    feat(insta-scrapper): added proxy support

--- kuwala/pipelines/insta-scrapper/src/app.py
+++ kuwala/pipelines/insta-scrapper/src/app.py
@@ -1,3 +1,8 @@
+
+import logging
+import sys
+import os
+
 from quart import Quart
 from routes.hashtag import hashtag
 from routes.posts import posts
@@ -5,6 +10,12 @@ from routes.profiles import profiles
 from routes.locations import locations
 from routes.reels import reels
 from routes.igtv import igtv
+
+from utils.proxy import check_proxy_connection
+
+from dotenv import load_dotenv
+
+logging.getLogger().setLevel(logging.INFO)
 
 app = Quart(__name__)
 app.register_blueprint(hashtag)
@@ -17,5 +28,14 @@ app.register_blueprint(igtv)
 @app.route('/')
 async def hello():
     return 'Running Insta Scrapper API on 3016'
+
+environment = sys.argv[1] if len(sys.argv) > 1 else 'local'
+logging.info(f'Running on {environment} environment')
+
+load_dotenv(dotenv_path=f'../config/env/.env.{environment}')
+
+if not check_proxy_connection():
+    logging.warning('Could not connect to proxy.')
+    
 
 app.run(host='0.0.0.0', port=3016)
