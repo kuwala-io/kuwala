f3d7e9ae616d26dc9b20823c8494e3bca899807a
Iga Narendra <iga.narendra@gmail.com>
2022-03-29 17:38:54 +0700

    wrapper in progress

--- kuwala/core/backend/app/models/robyn.py
+++ kuwala/core/backend/app/models/robyn.py
@@ -1,4 +1,7 @@
+from multiprocessing import context
+from operator import index
 import os
+from pickle import FALSE
 # load data form postgres:
 import psycopg2
 import pandas as pd
@@ -35,10 +38,12 @@ query =  '''
     '''
 #read query result to pandas dataframe
 marketing_data = sqlio.read_sql_query(query, conn, index_col='index')
+holiday_data = sqlio.read_sql_query('SELECT * FROM marketing_holiday_list', conn, index_col='index')
 temp_dir = '../../../../tmp/robyn/'
 if not os.path.exists(temp_dir):
     os.makedirs(temp_dir)
 marketing_data.to_csv(temp_dir+"marketing_data.csv")
+holiday_data.to_csv(temp_dir+'holiday_data.csv')
 
 print("\n loading Robyn...")
 # install.packages("remotes") 
@@ -52,8 +57,160 @@ from rpy2.robjects import pandas2ri
 
 pandas2ri.activate()
 
-robyn_object=temp_dir+"RobynObject.RDS"
+robyn_object=temp_dir+"MyRobyn.RDS"
 robyn = importr('Robyn')
-robyn_marketing_data = robjects.r('read.table(file = "{temp_dir}marketing_data.csv", header = T)'.format(temp_dir=temp_dir))
+dt_simulated_weekly = robjects.r('read.csv(file = "{temp_dir}marketing_data.csv", header = T )'.format(temp_dir=temp_dir))
+dt_prophet_holidays = robjects.r('read.csv(file  = "{temp_dir}holiday_data.csv", header = T )'.format(temp_dir=temp_dir))
+prophet_vars = robjects.r('c("trend", "season", "holiday")')
+context_vars = robjects.r('c("competitor_sales_B", "events")')
+paid_media_vars = robjects.r('c("tv_S", "ooh_S"	,	"print_S"	,"facebook_I" ,"search_clicks_P")')
+paid_media_spends = robjects.r('c("tv_S","ooh_S",	"print_S"	,"facebook_S", "search_S")')
+organic_vars = robjects.r('c("newsletter")')
+factor_vars = robjects.r('c("events")')
 
-#input_collector = robyn.robyn_inputs()
+
+robyn_inputs = robyn.robyn_inputs 
+
+# InputCollect_1 = robyn_inputs( 
+#    dt_input = dt_simulated_weekly
+#   ,dt_holidays = dt_prophet_holidays
+#   ,date_var = "date" # date format must be "2020-01-01"
+#   ,dep_var = "revenue" # there should be only one dependent variable
+#   ,dep_var_type = "revenue" # "revenue" or "conversion"
+#   ,prophet_vars = prophet_vars # "trend","season", "weekday" & "holiday"
+#   ,prophet_country = "DE"# input one country. dt_prophet_holidays includes 59 countries by default
+#   ,context_vars = context_vars # e.g. competitors, discount, unemployment etc
+#   ,paid_media_spends = paid_media_spends # mandatory input
+#   ,paid_media_vars = paid_media_vars # mandatory.
+#   # paid_media_vars must have same order as paid_media_spends. Use media exposure metrics like
+#   # impressions, GRP etc. If not applicable, use spend instead.
+#   ,organic_vars = organic_vars # marketing activity without media spend
+#   ,factor_vars = factor_vars # specify which variables in context_vars or organic_vars are factorial
+#   ,window_start = "2016-11-23"
+#   ,window_end = "2018-08-22"
+#   ,adstock = "geometric" # geometric, weibull_cdf or weibull_pdf.
+# )
+
+InputCollect=robjects.r(
+    '''
+  InputCollect <- robyn_inputs(
+  dt_input = dt_simulated_weekly
+  ,dt_holidays = dt_prophet_holidays
+  ,date_var = "DATE" # date format must be "2020-01-01"
+  ,dep_var = "revenue" # there should be only one dependent variable
+  ,dep_var_type = "revenue" # "revenue" or "conversion"
+  ,prophet_vars = c("trend", "season", "holiday") # "trend","season", "weekday" & "holiday"
+  ,prophet_country = "DE"# input one country. dt_prophet_holidays includes 59 countries by default
+  ,context_vars = c("competitor_sales_B", "events") # e.g. competitors, discount, unemployment etc
+  ,paid_media_spends = c("tv_S","ooh_S",	"print_S"	,"facebook_S", "search_S") # mandatory input
+  ,paid_media_vars = c("tv_S", "ooh_S"	,	"print_S"	,"facebook_I" ,"search_clicks_P") # mandatory.
+  # paid_media_vars must have same order as paid_media_spends. Use media exposure metrics like
+  # impressions, GRP etc. If not applicable, use spend instead.
+  ,organic_vars = c("newsletter") # marketing activity without media spend
+  ,factor_vars = c("events") # specify which variables in context_vars or organic_vars are factorial
+  ,window_start = "2016-11-23"
+  ,window_end = "2018-08-22"
+  ,adstock = "geometric" # geometric, weibull_cdf or weibull_pdf.
+)
+
+'''
+)
+
+print("The InputCollect contains:")
+print(InputCollect)
+
+adstock = robjects.r.list('geometric') # geometric, weibull_cdf or weibull_pdf. make it same with before
+all_media = robjects.r('c("tv_S","ooh_S","print_S","facebook_S", "search_S", "newsletter")')
+# all_media = paid_media_spedns + organic_vars
+
+#hyper_names= robyn.hyper_names(adstock = adstock, all_media = all_media)
+hyper_names = robyn.hyper_names(adstock = robjects.r('InputCollect$adstock'), all_media = robjects.r('InputCollect$all_media'))
+hyper_names = robjects.r ('hyper_names(adstock = InputCollect$adstock, all_media = InputCollect$all_media)')
+
+robyn.plot_adstock = FALSE
+robyn.plot_stauration = FALSE
+
+hyperparameters = robjects.r (
+    '''
+   hyperparameters <- list(
+  facebook_S_alphas = c(0.5, 3)
+  ,facebook_S_gammas = c(0.3, 1)
+  ,facebook_S_thetas = c(0, 0.3)
+
+  ,print_S_alphas = c(0.5, 3)
+  ,print_S_gammas = c(0.3, 1)
+  ,print_S_thetas = c(0.1, 0.4)
+
+  ,tv_S_alphas = c(0.5, 3)
+  ,tv_S_gammas = c(0.3, 1)
+  ,tv_S_thetas = c(0.3, 0.8)
+
+  ,search_S_alphas = c(0.5, 3)
+  ,search_S_gammas = c(0.3, 1)
+  ,search_S_thetas = c(0, 0.3)
+
+  ,ooh_S_alphas = c(0.5, 3)
+  ,ooh_S_gammas = c(0.3, 1)
+  ,ooh_S_thetas = c(0.1, 0.4)
+
+  ,newsletter_alphas = c(0.5, 3)
+  ,newsletter_gammas = c(0.3, 1)
+  ,newsletter_thetas = c(0.1, 0.4)
+  )
+  
+    '''
+)
+
+# hyperparameters = robjects.ListVector 
+# ({
+#     'facebook_S_alphas': {0.5,3}, 
+#     'facebook_S_gammas': {0.3,1}, 
+#     'facebook_S_thetas': {0,0.3},
+#     'print_S_alphas': {0.5,3}, 
+#     'print_S_gammas': {0.3,1}, 
+#     'print_S_thetas': {0.1,0.4},
+#     'tv_S_alphas': {0.5,3}, 
+#     'tv_S_gammas': {0.3,1}, 
+#     'tv_S_thetas': {0.3,0.8},
+#     'search_S_alphas': {0.5,3}, 
+#     'search_S_gammas': {0.3,1}, 
+#     'search_S_thetas': {0,0.3},
+#     'ooh_S_alphas': {0.5,3}, 
+#     'ooh_S_gammas': {0.3,1}, 
+#     'ooh_S_thetas': {0.1,0.4},
+#     'newsletter_alphas': {0.5,3}, 
+#     'newsletter_gammas': {0.3,1},
+#     'newsletter_thetas': {0.1,0.4}
+# })
+
+#print(robjects.r('order(names(hyperparameters))'))
+# print(robjects.r.list(InputCollect_1[0]))
+#print(robjects.r('print(hyperparameters)'))
+InputCollect = robyn_inputs(InputCollect = InputCollect, hyperparameters = hyperparameters)
+#InputCollect = robjects.r ('InputCollect <- robyn_inputs(InputCollect = InputCollect, hyperparameters = hyperparameters')
+print(InputCollect)
+robyn_run =  robyn.robyn_run
+
+# OutputModels = robyn_run(
+#     InputCollect = InputCollect # feed in all model specification
+#   #, cores = NULL # default
+#   #, add_penalty_factor = FALSE # Untested feature. Use with caution.
+#   , iterations = 2000 # recommended for the dummy dataset
+#   , trials = 5 # recommended for the dummy dataset
+#   , outputs = FALSE # outputs = FALSE disables direct model output
+# )
+
+OutputModels1= robjects.r (
+    '''
+    InputCollect <- robyn_inputs(InputCollect = InputCollect, hyperparameters = hyperparameters)
+    OutputModels <- robyn_run(
+        InputCollect = InputCollect # feed in all model specification
+        #, cores = NULL # default
+        #, add_penalty_factor = FALSE # Untested feature. Use with caution.
+        , iterations = 2000 # recommended for the dummy dataset
+        , trials = 5 # recommended for the dummy dataset
+        , outputs = FALSE # outputs = FALSE disables direct model output
+)
+'''
+)
+print(OutputModels1)
